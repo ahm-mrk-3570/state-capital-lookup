c9d81db45d9faf746eb78b3e5db8fa41fa521198
AHM MRK <ahmmrk.3570m@gmail.com>
2026-09-11 20:43:10 +0330

    Updates

--- assets/index-Ct0HIhP9.js
+++
@@ -1,6 +0,0 @@
-(function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin===`use-credentials`?t.credentials=`include`:e.crossOrigin===`anonymous`?t.credentials=`omit`:t.credentials=`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();var e=document.querySelector(`#input`),t=document.querySelector(`#output`),n=async e=>{let n=(await(await fetch(`/js-state-capital-lookup//data/states.json`)).json()).filter(t=>{let n=RegExp(`^${e}`,`gi`);return t.name.match(n)||t.abbr.match(n)}),r=``;n.map(e=>{r+=`
-      <div class="card">
-          <h4 class="title-card">${e.name} (${e.abbr}) <span class="capital-card">${e.capital}</span></h4>
-          <small>Lat : ${e.lat} / Long : ${e.long}</small>
-      </div>
-    `}),t.innerHTML=r,e===``&&(t.innerHTML=``)};e.addEventListener(`input`,()=>n(e.value));
